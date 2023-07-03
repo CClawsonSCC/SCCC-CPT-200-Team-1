939e3a80c0c98dd4f326bea0730fd0257c836097
assertX <assert0ffical.help@gmail.com>
2023-07-03 14:50:49 -0500

    Some Table layout updates

--- Code-Crate/Program.cs
+++ Code-Crate/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using CodeCrateData;
 using Code_Crate.Data;
+using BlazorBootstrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +14,7 @@ builder.Services.AddScoped<UserAccountService>();
 builder.Services.AddSingleton<UserAccountDataCsv>();
 builder.Services.AddScoped<PasswordLogService>();
 builder.Services.AddSingleton<PasswordLogDataCsv>();
-
+builder.Services.AddBlazorBootstrap();
 
 
 var app = builder.Build();
