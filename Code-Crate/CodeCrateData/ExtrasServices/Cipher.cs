2da3b76c6378440afbe006a3becf5640f8f58f15
ZackLynx <71418708+CClawsonSCC@users.noreply.github.com>
2023-07-16 13:05:00 -0500

    Bitwise encoder implemented

--- Code-Crate/CodeCrateData/ExtrasServices/Cipher.cs
+++ Code-Crate/CodeCrateData/ExtrasServices/Cipher.cs
@@ -9,15 +9,16 @@ namespace CodeCrateData {
 
   public class Cipher
   {
-    private ArrayList _key = new ArrayList() { 1, 2, 3, 4, 5, 6};
+    private ArrayList _key = new ArrayList() { 1, 2, 3, 4, 5, 6 };
+    private ushort[] keyList = { 1, 2, 3, 4, 5, 6 };
     private int _keySize = 6;
     
     /* -- Theory --
     * Type conversion is necessary for this to work.
-    * Character by character, convert <char> to <uint>
+    * Character by character, convert <char> to <ushort>
     * the bit shift operation is as follows:
-    * ((uint_Character_Value << degree_Of_Rotation) | (uint_Character_Value >> (32 - degree_Of_Rotation)))
-    * convert <uint> back to <char>
+    * ((ushort_Character_Value << degree_Of_Rotation) | (ushort_Character_Value >> (32 - degree_Of_Rotation)))
+    * convert <ushort> back to <char>
     * append each <char> in order to the string
     */
     
@@ -33,9 +34,16 @@ namespace CodeCrateData {
 
       for (int j = 0; j < inString.Length; j++)
       {
+        /*
         int digit = Convert.ToInt32(inString[j]);
         digit += _key.IndexOf(i);
         outString += (Convert.ToChar(digit));
+        */
+
+        ushort digit = Convert.ToUInt16(inString[j]);
+        digit = (ushort)((digit << keyList[i]) | digit >> (16 - keyList[i]));
+        outString += (Convert.ToChar(digit));
+
         i = (i == _keySize - 1) ? 0 : i + 1;
       }
       return outString;
@@ -53,9 +61,16 @@ namespace CodeCrateData {
       
       for (int j = 0; j < inString.Length; j++)
       {
+        /*         
         int digit = Convert.ToInt32(inString[j]);
         digit -= _key.IndexOf(i);
         outString += (Convert.ToChar(digit));
+        */
+        
+        ushort digit = Convert.ToUInt16(inString[j]);
+        digit = (ushort)((digit >> keyList[i]) | digit << (16 - keyList[i]));
+        outString += (Convert.ToChar(digit));
+
         i = (i == _keySize - 1) ? 0 : i + 1;
       }
       return outString;
